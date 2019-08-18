c1de05938d275e2069d5b622c14d8ec09c7d1509
Maxim Berge <maxim.berge@gmail.com>
2019-08-19 01:19:04 +0200

    comment Contact OK

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,6 +3,11 @@ import React, {Component, Fragment} from 'react';
 // Styles
 import './Contact.scss'
 
+// Pictures
+import mail from '../../assets/img/mail.png'
+import phone from '../../assets/img/phone.png'
+import map from '../../assets/img/map.png'
+
 // Components
 import PageTitle from "../PageTitle/PageTitle";
 
@@ -13,24 +18,28 @@ export default class Contact extends Component {
             <Fragment>
                 <main className='bg-contact'>
                     <PageTitle title='Contact' divider='divider-white' />
-                    <div>
-                        <section>
+                    <div className='contact-container'>
+                        <section className='form-container'>
                             <form action="">
-                                <label htmlFor=""><input type="text"/></label>
-                                <label htmlFor=""><input type="text"/></label>
-                                <label htmlFor=""><input type="text-area"/></label>
-                                <input type="submit" value='Envoyer' />
+                                <label htmlFor="">Nom<input type="text"/></label>
+                                <label htmlFor="">Adresse mail<input type="text"/></label>
+                                <label htmlFor="">Message<textarea /></label>
+                                <input className='submit' type="submit" value='Envoyer' />
                             </form>
                         </section>
-                        <section>
+                        <section className='info-container'>
                             <ul>
-                                <li> DanieleSnijers@gmail.com </li>
-                                <li> 0496 / 456 125 </li>
-                                <li> Avenue Eugène Mascaux 781, 6001, Marcinelle </li>
+                                <li><img src={mail} alt="mail"/> <p> DanieleSnijers@gmail.com </p></li>
+                                <li><img src={phone} alt="téléphone"/> <p> 0496 / 456 125 </p></li>
+                                <li><img src={map} alt="carte"/> <p> Avenue Eugène Mascaux 781, 6001, Marcinelle </p></li>
                             </ul>
                         </section>
                     </div>
                 </main>
+                <section>
+                    <iframe className='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2544.113816295115!2d4.42704241584942!3d50.38307680009807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2242bfbc4217b%3A0xac93b985ef300a19!2sArt+and+Dogs!5e0!3m2!1sfr!2sbe!4v1566169711322!5m2!1sfr!2sbe"
+                        width="100%" height="450" allowFullScreen />
+                </section>
             </Fragment>
         );
     }
