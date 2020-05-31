cf698118c7434cf98ca8dfc466e8c045c593c3d3
maxim berge <maxim.berge@gmail.com>
2020-05-31 05:59:58 +0200

    add undifined if condiitons false in ternary condition in NavBarLarge

--- website/src/components/Navbar/NavbarLarge.jsx
+++ website/src/components/Navbar/NavbarLarge.jsx
@@ -32,14 +32,14 @@ export default class NavbarLarge extends Component {
     return (
       <Router>
         <nav className='nav-large'>
-          <h1 id={this.state.activePage === '' && 'active'}><Link onClick={() => this.updateActivePage('')} to='/'>Art & Dogs</Link></h1>
+          <h1 id={this.state.activePage === '' ? 'active' : undefined}><Link onClick={() => this.updateActivePage('')} to='/'>Art & Dogs</Link></h1>
           <ul>
-            <li id={this.state.activePage === 'services' && 'active'} > <Link onClick={() => this.updateActivePage('services')} to='/services' >  Services </Link> </li>
-            <li id={this.state.activePage === 'tarif' && 'active'} > <Link onClick={() => this.updateActivePage('tarif')} to='/tarif' > Tarif </Link> </li>
-            <li id={this.state.activePage === 'team' && 'active'} > <Link onClick={() => this.updateActivePage('team')} to='/team' > L'équipe </Link> </li>
-            <li id={this.state.activePage === 'gallery' && 'active'} > <Link onClick={() => this.updateActivePage('gallery')} to='/gallery' > Galerie </Link> </li>
-            <li id={this.state.activePage === 'contact' && 'active'} > <Link onClick={() => this.updateActivePage('contact')} to='/contact' > Contact </Link> </li>
-            <li id={this.state.activePage === 'hours' && 'active'} > <Link onClick={() => this.updateActivePage('hours')} to='/hours' > Horaire </Link> </li>
+            <li id={this.state.activePage === 'services' ? 'active' : undefined} > <Link onClick={() => this.updateActivePage('services')} to='/services' >  Services </Link> </li>
+            <li id={this.state.activePage === 'tarif' ? 'active' : undefined} > <Link onClick={() => this.updateActivePage('tarif')} to='/tarif' > Tarif </Link> </li>
+            <li id={this.state.activePage === 'team' ? 'active' : undefined} > <Link onClick={() => this.updateActivePage('team')} to='/team' > L'équipe </Link> </li>
+            <li id={this.state.activePage === 'gallery' ? 'active' : undefined} > <Link onClick={() => this.updateActivePage('gallery')} to='/gallery' > Galerie </Link> </li>
+            <li id={this.state.activePage === 'contact' ? 'active' : undefined} > <Link onClick={() => this.updateActivePage('contact')} to='/contact' > Contact </Link> </li>
+            <li id={this.state.activePage === 'hours' ? 'active' : undefined} > <Link onClick={() => this.updateActivePage('hours')} to='/hours' > Horaire </Link> </li>
           </ul>
         </nav>
         <Route exact path='/' component={Home} />
